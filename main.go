8bf8756f17090bb0e8c7006420aac5d038468b9e
Robert Schuh <robert.schuh@valtech.de>
2021-12-09 16:29:06 +0100

    add some logic for pod ips

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/robbilie/service-call-multiplexer/logger"
+	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/client-go/kubernetes"
@@ -11,6 +12,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"sync"
 	"time"
 )
 
@@ -45,6 +47,7 @@ func main() {
 		getenv("SERVICE_NAMESPACE", "ingress-nginx"),
 		getenv("SERVICE_NAME", "ingress-nginx-controller"),
 		getenv("SERVICE_PORT", "80"),
+		getenv("SERVICE_PROTOCOL", "http"),
 	)
 	if err != nil {
 		loggerInstance.Fatalw("Couldn't initialize server", "err", err)
@@ -66,34 +69,43 @@ type server struct {
 	Namespace     string
 	LabelSelector string
 	Port          string
+	Protocol      string
+	ClientSet     *kubernetes.Clientset
+	HttpClient    http.Client
 	Logger        logger.Logger
 }
 
-func newServer(logger logger.Logger, serviceNamespace string, serviceName string, servicePort string) (*server, error) {
+func newServer(logger logger.Logger, serviceNamespace string, serviceName string, servicePort string, serviceProtocol string) (*server, error) {
 	config, err := rest.InClusterConfig()
 	if err != nil {
 		panic(err.Error())
 	}
-	clientset, err := kubernetes.NewForConfig(config)
+	clientSet, err := kubernetes.NewForConfig(config)
 	if err != nil {
 		panic(err.Error())
 	}
-	service, err := clientset.CoreV1().Services(serviceNamespace).Get(context.TODO(), serviceName, metav1.GetOptions{})
+	service, err := clientSet.CoreV1().Services(serviceNamespace).Get(context.TODO(), serviceName, metav1.GetOptions{})
 	if err != nil {
 		panic(err.Error())
 	}
 	set := labels.Set(service.Spec.Selector)
-	pods, err := clientset.CoreV1().Pods(serviceNamespace).List(context.TODO(), metav1.ListOptions{LabelSelector: set.AsSelector().String()})
+	pods, err := clientSet.CoreV1().Pods(serviceNamespace).List(context.TODO(), metav1.ListOptions{LabelSelector: set.AsSelector().String()})
 	if err != nil {
 		panic(err.Error())
 	}
 	for _, pod := range pods.Items {
 		log.Println(pod.GetName(), pod.Spec.NodeName, pod.Spec.Containers)
 	}
+	client := http.Client{
+		Timeout: time.Duration(10 * time.Second),
+	}
 	return &server{
 		Namespace:     serviceNamespace,
 		LabelSelector: set.AsSelector().String(),
 		Port:          servicePort,
+		Protocol:      serviceProtocol,
+		ClientSet:     clientSet,
+		HttpClient:    client,
 		Logger:        logger,
 	}, nil
 }
@@ -108,5 +120,42 @@ func getenv(key, fallback string) string {
 
 func (s *server) handleRequest(rw http.ResponseWriter, r *http.Request) {
 	s.Logger.Debugw("Handled validation request", "method", r.Method, "url", r.URL, "status", http.StatusNoContent, "method", r.Method, "userAgent", r.UserAgent())
+
+	pods, err := s.ClientSet.CoreV1().Pods(s.Namespace).List(context.TODO(), metav1.ListOptions{LabelSelector: s.LabelSelector})
+	if err != nil {
+		panic(err.Error())
+	}
+
+	ch := make(chan int)
+	var wg sync.WaitGroup
+	for _, pod := range pods.Items {
+		wg.Add(1)
+		go s.makeCall(pod, r, ch, &wg)
+	}
+
+	go func() {
+		wg.Wait()
+		close(ch)
+	}()
+
+	for statusCode := range ch {
+		if statusCode < 100 || statusCode >= 300 {
+			panic(err.Error())
+		}
+	}
 	rw.WriteHeader(http.StatusNoContent)
+}
+
+func (s *server) makeCall(pod v1.Pod, r *http.Request, ch chan<- int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	log.Println(pod.GetName(), pod.Spec.NodeName, pod.Spec.Containers)
+	request, err := http.NewRequest(r.Method, s.Protocol+"://"+pod.Status.PodIP+r.URL.Path, nil)
+	if err != nil {
+		panic(err.Error())
+	}
+	response, err := s.HttpClient.Do(request)
+	if err != nil {
+		panic(err.Error())
+	}
+	ch <- response.StatusCode
 }
